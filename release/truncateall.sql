55afd9aa4b3e94ed0731db28555230bde887e463
Carlos Velasquez <imadogsi64@gmail.com>
2018-12-17 07:35:49 -0600

    stupido

---
+++ release/truncateall.sql
@@ -0,0 +1,13 @@
+TRUNCATE TABLE Categories;
+TRUNCATE TABLE CustomerCustomerDemo;
+TRUNCATE TABLE CustomerDemographics;
+TRUNCATE TABLE Customers;
+TRUNCATE TABLE EmployeeTerritories;
+TRUNCATE TABLE Employees;
+TRUNCATE TABLE 'Order Details';
+TRUNCATE TABLE Orders;
+TRUNCATE TABLE Products;
+TRUNCATE TABLE Region;
+TRUNCATE TABLE Shippers;
+TRUNCATE TABLE Suppliers;
+TRUNCATE TABLE Territories;
